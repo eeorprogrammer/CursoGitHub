d1e2961308dc787a792ccfad1ee4ae3a5fca2721
Edgar Ontiveros <ontiverosedgare@johndeere.com>
2021-01-17 19:36:20 -0600

    fix another change 2

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 /*
 Este es un comentario en el archivo de JavaScript
-=======
-/*
-Este es un comentario en el archivo de JavaScript
->>>>>>> 3c86f71e13218c17e0d96d45dbe90e2d38bdb816
 */
